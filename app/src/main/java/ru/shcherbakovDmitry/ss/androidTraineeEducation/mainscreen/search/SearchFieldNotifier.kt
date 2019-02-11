e45bf0daa1d062cb0a3b13c73f52507409807f17
Shcherbakov Dmitry <shcherbakov_dmitry@bk.ru>
2019-02-11 20:23:01 +0300

    Исправил ошибку Улучшил логику генерации результатов по перелистыванию

--- app/src/main/java/ru/shcherbakovDmitry/ss/androidTraineeEducation/mainscreen/search/SearchFieldNotifier.kt
+++ app/src/main/java/ru/shcherbakovDmitry/ss/androidTraineeEducation/mainscreen/search/SearchFieldNotifier.kt
@@ -2,7 +2,12 @@ package ru.shcherbakovDmitry.ss.androidTraineeEducation.mainscreen.search
 
 object SearchFieldNotifier {
 
+    var inactiveItem = 1
     var activeItem = 0
+        set(value){
+            inactiveItem = field
+            field = value
+        }
     private val listOfObservers: ArrayList<SearchFieldObserver> = ArrayList()
 
     fun attachObserver(observer: SearchFieldObserver) {
@@ -14,6 +19,6 @@ object SearchFieldNotifier {
     }
 
     fun findContent(key: String?) {
-        listOfObservers[activeItem].requestContent(key)
+        listOfObservers[inactiveItem].requestContent(key)
     }
 }
