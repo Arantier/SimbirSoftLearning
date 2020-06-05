04257455bbb2ac08f23a28ff6c68ef8c707a5c6a
Shcherbakov <shcherbakov_dmitry@bk.ru>
2020-06-06 00:28:02 +0300

    Switched data source from storage to net But charities are being parsed incorrectly

--- app/src/main/java/ru/shcherbakovdv/ss/trainee/SplashActivity.kt
+++ app/src/main/java/ru/shcherbakovdv/ss/trainee/SplashActivity.kt
@@ -1,94 +1,123 @@
 package ru.shcherbakovdv.ss.trainee
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.gson.Gson
-import io.reactivex.Single
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
-import org.threeten.bp.format.DateTimeFormatter
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import ru.shcherbakovdv.ss.trainee.data.Charity
+import io.realm.RealmObject
 import ru.shcherbakovdv.ss.trainee.data.RealmCategory
 import ru.shcherbakovdv.ss.trainee.data.RealmCharity
-import ru.shcherbakovdv.ss.trainee.utilites.json.JsonUtils
-import java.io.IOException
-import java.io.InputStream
-import java.lang.RuntimeException
+import ru.shcherbakovdv.ss.trainee.utilites.Logger
 
 class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
-
-    private lateinit var realm: Realm
-
-    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Realm.init(this)
-        realm = Realm.getDefaultInstance()
-        realm.executeTransaction { realm ->
-            realm.deleteAll()
-        }
 
-        val isStorageAvailable = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-        if (isStorageAvailable) {
-            disposable = loadAndSave()
-                .subscribe({
-                    startActivity(Intent(this, MainActivity::class.java))
-                }, { t -> showErrorMessage() })
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                STORAGE_REQUEST_CODE
+        Completable.create {
+            Realm.getDefaultInstance()
+                .apply {
+                    executeTransaction { it.deleteAll() }
+                    close()
+                }
+        }.subscribeOn(Schedulers.io()).subscribe()
+
+        updateData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { startActivity(Intent(this, MainActivity::class.java)) },
+                { error ->
+                    showErrorMessage(error)
+                }
             )
+
+    }
+
+    private fun updateData(): Completable {
+        val categoriesCompletable = loadCategories()
+            .saveToRealm()
+            .let { Completable.fromObservable(it) }
+        val charitiesCompletable = loadCharities()
+            .saveToRealm()
+            .let { Completable.fromObservable(it) }
+
+        return categoriesCompletable
+            .andThen(charitiesCompletable)
+    }
+
+    private fun loadCategories(): Observable<RealmCategory> =
+        Observable.create<RealmCategory> { emitter ->
+            FirebaseDatabase.getInstance()
+                .getReference("categories")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        snapshot.children.forEach {
+                            it.getValue(RealmCategory::class.java)
+                                ?.let { emitter.onNext(it) }
+                                ?: emitter.onError(NullPointerException("Null pointer exception occured while retrieving data from net"))
+                        }
+                        emitter.onComplete()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        emitter.onError(error.toException())
+                    }
+                })
         }
 
-    }
+    private fun loadCharities(): Observable<RealmCharity> =
+        Observable.create { emitter ->
+            FirebaseDatabase.getInstance()
+                .getReference("categories")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        snapshot.children.forEach {
+                            it.getValue(RealmCharity::class.java)
+                                ?.let { emitter.onNext(it) }
+                                ?: emitter.onError(NullPointerException("Null pointer exception occured while retrieving data from net"))
+                        }
+                        emitter.onComplete()
+                    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        disposable.dispose()
-    }
+                    override fun onCancelled(error: DatabaseError) {
+                        emitter.onError(error.toException())
+                    }
+                })
+        }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            STORAGE_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    disposable = loadAndSave()
-                        .subscribe({
-                            startActivity(Intent(this, MainActivity::class.java))
-                        }, { t -> showErrorMessage() })
-                } else {
-                    Log.e(LOG_TAG, "Storage permission wasn't granted")
-                    showErrorMessage()
+    private fun Observable<out RealmObject>.saveToRealm(): Observable<out RealmObject> {
+        var realm: Realm? = null
+        return this
+            .observeOn(Schedulers.io())
+            .doOnEach {
+                if (realm == null) {
+                    realm = Realm.getDefaultInstance()
+                    realm?.beginTransaction()
                 }
             }
-        }
+            .doOnNext { realm?.copyToRealm(it) }.doOnComplete { realm?.commitTransaction() }
+            .doOnError { realm?.cancelTransaction() }.doOnTerminate {
+                realm?.close()
+            }.observeOn(AndroidSchedulers.mainThread())
     }
 
-    private fun showErrorMessage() {
+
+    private fun showErrorMessage(throwable: Throwable) {
+        // TODO: раздели ошибки по категориям и сделай отдельный вывод для каждой
+        Logger.flatError(throwable.message)
         // TODO: перенеси это в другое место
         AlertDialog.Builder(this)
             .setTitle(R.string.title_error)
@@ -102,99 +131,5 @@ class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
             .create()
             .show()
 
-    }
-
-    fun getGithubRetrofit() = Retrofit.Builder()
-        .baseUrl("https://raw.githubusercontent.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .build()
-
-    private fun loadData(): Single<Pair<Array<RealmCategory>, Array<RealmCharity>>> =
-        Single.create<Pair<InputStream, InputStream>> {
-            try {
-                it.onSuccess(
-                    Pair(
-                        assets.open("categories.json"),
-                        assets.open("charities.json")
-                    )
-                )
-            } catch (exception: IOException) {
-                it.onError(exception)
-            }
-        }.flatMap { filePair ->
-            val categoriesData = filePair.first.let {
-                val size = it.available()
-                val buffer = ByteArray(size)
-                it.read(buffer)
-                it.close()
-                String(buffer)
-            }
-
-            val charitiesData = filePair.second.let {
-                val size = it.available()
-                val buffer = ByteArray(size)
-                it.read(buffer)
-                it.close()
-                String(buffer)
-            }
-
-            val categoriesArray = Gson().fromJson<Array<RealmCategory>>(
-                categoriesData,
-                Array<RealmCategory>::class.java
-            )
-            val charitiesArray = JsonUtils.gson.fromJson(charitiesData, Array<Charity>::class.java)
-                .let { array ->
-                    val realmCharitiesList = ArrayList<RealmCharity>()
-                    for (charity in array) {
-                        realmCharitiesList.add(
-                            RealmCharity(
-                                charity.categoryId,
-                                charity.title,
-                                charity.description,
-                                JsonUtils.toJson(charity.picturesUrls),
-                                charity.startDate.format(DateTimeFormatter.ISO_DATE),
-                                charity.endDate.format(DateTimeFormatter.ISO_DATE),
-                                JsonUtils.toJson(charity.organisation),
-                                JsonUtils.toJson(charity.donatorsPicturesUrls)
-                            )
-                        )
-                    }
-                    realmCharitiesList.toTypedArray()
-                }
-
-            Single.just(Pair(categoriesArray, charitiesArray))
-        }
-
-    private fun saveDataToRealm(pair: Pair<Array<RealmCategory>, Array<RealmCharity>>): Single<Int> {
-        val categoriesArray = pair.first
-        val charitiesArray = pair.second
-
-        realm = Realm.getDefaultInstance()
-        realm.beginTransaction()
-
-        for (category in categoriesArray) {
-            realm.copyToRealm(category)
-        }
-
-        for (charity in charitiesArray) {
-            realm.copyToRealm(charity)
-        }
-
-        realm.commitTransaction()
-        realm.close()
-
-        return Single.just(1)
-    }
-
-    private fun loadAndSave(): Single<Int> =
-        loadData().flatMap(this::saveDataToRealm)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-
-    companion object {
-        const val LOG_TAG = "Splash Activity"
-
-        const val STORAGE_REQUEST_CODE = 1
     }
 }
--- app/src/main/java/ru/shcherbakovdv/ss/trainee/utilites/Logger.kt
+++ app/src/main/java/ru/shcherbakovdv/ss/trainee/utilites/Logger.kt
@@ -7,11 +7,11 @@ object Logger {
     private const val FLAT_DEBUG_VALUE = "flatdebugtag"
     private const val FLAT_ERROR_VALUE = "flaterrortag"
 
-    fun flatDebug(message:String){
+    fun flatDebug(message:String?){
         Log.d(FLAT_DEBUG_VALUE,message)
     }
 
-    fun flatError(message:String){
+    fun flatError(message:String?){
         Log.e(FLAT_ERROR_VALUE,message)
     }
 }
